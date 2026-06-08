7c8b32a29666248c07bd48320db9c097720d0335
snagy11224-cmyk <snagy11224@gmail.com>
2026-06-09 01:18:14 +0300

    feat: craeate password resets table/migration

--- src/migrations/20260606212547_create_password_reset_table.ts
+++ src/migrations/20260606212547_create_password_reset_table.ts
@@ -2,9 +2,25 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+await knex.raw(`
+CREATE TABLE password_resets (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL references users(id) on delete cascade,
+    otp_hash text NOT NULL,
+    expires_at timestamp NOT NULL,
+    consumed_at timestamp not null,
+    created_at timestamp not null,
+    constraint fk_password_resets_user_id foreign key (user_id) references users(id) 
+);
+
+create index idx_password_resets_user_id on password_resets(user_id);
+`)
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.raw(`
+    DROP TABLE password_resets;
+    `)
 }
 
