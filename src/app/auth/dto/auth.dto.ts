e97d4e2c23bd75b9d56073650688883dcff08a77
snagy11224-cmyk <snagy11224@gmail.com>
2026-06-09 20:37:14 +0300

    feat: Defined auth dto

---
+++ src/app/auth/dto/auth.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsString, IsStrongPassword, MaxLength, MinLength } from "class-validator";
+
+export class registerDto{
+    @IsEmail()
+    email!: string;
+
+    @MinLength(10)
+    @MaxLength(11)
+    phone!: string
+
+    @IsString()
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    },{message:"Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number and one symbol"})
+    password!: string;
+
+    @IsString()
+    @MinLength(1)
+    name!: string;
+}
---
+++ src/app/auth/service/auth.service.ts
@@ -0,0 +1,15 @@
+export class AuthService {
+register :(data: Object)=>Promise<void> =async (data:Object): Promise<void> => { 
+//1. check if user with email or phone already exists, if yes throw error
+
+//2. else hash the password
+
+//3. create user in database
+
+//4. create jwt token and return to user
+
+//5. return user data and tokens
+
+  }
+    
+}
